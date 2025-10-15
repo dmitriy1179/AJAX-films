da1040ac2369babb4b8762b384d8072c6465de81
dmitriy1179 <chda7911@gmail.com>
2025-10-15 21:44:33 +0300

    add showFilms

--- sript.js
+++ sript.js
@@ -1,29 +1,40 @@
-const API = "http://www.omdbai.com";
+const API = "http://www.omdbapi.com";
 const key = "888cc6de";
 const ENDPOINT = `${API}/?apikey=${key}`;
-console.log("ENDPOINT", ENDPOINT);
 const searchBtn = document.getElementById("searchBtn");
 const form = document.getElementById("form");
+const list = document.getElementById("list");
 
 
-async function searchFilms(query) {
+async function searchFilms(query, type) {
   try {
-    const url = `${ENDPOINT}&s=${query}`
+    const url = `${ENDPOINT}&s=${query}&type=${type}`
     const response = await fetch(url);
-    console.log("response", response)
     if (!response.ok) {
       throw new Error(response.statusText)
     };
     const resultFilms = await response.json();
     console.log("result", resultFilms)
+    return resultFilms
   } catch (e) {
-    console.log("error", e);
+    console.error(e);
   };
 }
 
-
-searchBtn.onclick = (e) => {
+searchBtn.onclick = async (e) => {
+  const data = Object.fromEntries(new FormData(form))
   e.preventDefault();
-  searchFilms(`matrix`);
+
+  const query = form.query.value.trim();
+  const type = form.category.value;
+  if (!query) return;
+
+  const films = await searchFilms(query, type);
+  showFilms(films);
 }
 
+function showFilms(films) {
+  films.Search.forEach(film => {
+    list.innerHTML += `<li>${film.Title}</li>`
+  })
+}
